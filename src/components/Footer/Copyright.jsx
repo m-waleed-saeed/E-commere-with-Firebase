8c5de3d126520d6d3490e50864c3c29b2410bf7c
Muhammad Waleed <weedimian@gmail.com>
2025-09-27 15:38:12 +0500

    Change in Footer

--- src/components/Footer/Copyright.jsx
+++ src/components/Footer/Copyright.jsx
@@ -5,7 +5,7 @@ const Copyright = () => {
 
   return (
     <div className="text-center py-4 text-sm text-white font-medium bg-[#121212]">
-      &copy; {year} <span className="text-[#BB86FC]">Luxury Tech</span>. All rights reserved.
+      &copy; {year} All rights reserved. Developed by <span className='text-[#BB86FC]'>Muhammad Waleed</span>
     </div>
   );
 };
